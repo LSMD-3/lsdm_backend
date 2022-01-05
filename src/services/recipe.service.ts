527439c713967db88d06a887d1b3e076b03c2cdc
Marco Bellia <ing.bellia@gmail.com>
2022-01-05 19:06:06 +0100

    fix recipe and restaurants

--- src/services/recipe.service.ts
+++ src/services/recipe.service.ts
@@ -1,10 +1,15 @@
 import { IRecipe, RECIPE_PERMISSIONS } from '@/interfaces'
 import { Recipe } from '@/models'
-import { AbstractService } from './abstract.service'
+import { AbstractService, ProjectType } from './abstract.service'
 
 class RecipeService extends AbstractService<IRecipe> {
   public getEntityManager = () => Recipe
   public getPermissions = () => RECIPE_PERMISSIONS
+  public projectFieldsSearch = (type: ProjectType) => {
+    if (type === 'base') return 'all'
+    return '_id'
+  }
+  blackListUpdateFields = {}
 }
 
 export default new RecipeService()
--- src/services/restaurant.service.ts
+++ src/services/restaurant.service.ts
@@ -1,10 +1,15 @@
 import { IRestaurant, RESTAURANT_PERMISSIONS } from '@/interfaces'
 import { Restaurant } from '@/models'
-import { AbstractService } from './abstract.service'
+import { AbstractService, ProjectType } from './abstract.service'
 
 class RestaurantService extends AbstractService<IRestaurant> {
   public getEntityManager = () => Restaurant
   public getPermissions = () => RESTAURANT_PERMISSIONS
+  public projectFieldsSearch = (type: ProjectType) => {
+    if (type === 'base') return 'all'
+    return '_id'
+  }
+  blackListUpdateFields = { latitudine: 1, longitudine: 1 }
 }
 
 export default new RestaurantService()
